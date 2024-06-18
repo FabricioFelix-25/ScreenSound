cadb51842d478176fbdebfca0f0cdc38c849fc03
Fabricio Felix <fgb876alinhados@gmail.com>
2024-06-17 21:32:18 -0300

    herança

---
+++ ScreenSound/Menus/MenuExibirTituloDaOpcao.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuExibirTituloDaOpcao : Menu
+{
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
+    }
+}
