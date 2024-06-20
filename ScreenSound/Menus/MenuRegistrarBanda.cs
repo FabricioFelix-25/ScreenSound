cef1a7b293328403961f607e843d89a7817d611d
Fabricio Felix <felixfabricio115@gmail.com>
2024-06-20 10:09:20 -0300

    final

--- ScreenSound/Menus/MenuRegistrarBanda.cs
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -1,5 +1,5 @@
 ﻿
-
+using OpenAI_API;
 using ScreenSound.Modelos;
 
 namespace ScreenSound.Menus;
@@ -14,15 +14,24 @@ internal class MenuRegistrarBanda : Menu
         string nomeDaBanda = Console.ReadLine()!;
         Banda banda = new Banda(nomeDaBanda);
         bandasRegistradas.Add(nomeDaBanda, banda);
+        try
+        {
+            var client = new OpenAIAPI("<SUA API KEY AQUI>");
+            var chat = client.Chat.CreateConversation();
+            chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal");
+            var resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+            banda.Resumo = resposta;
 
-        var client = new OpenAIAPI("<SUA API KEY AQUI>");
-        var chat = client.Chat.CreateConversation();
-        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal");
-        var resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
-        banda.Resumo = resposta;
 
-        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocorreu um erro ao tentar registrar a banda ou obter o resumo.");
+            Console.WriteLine($"Detalhes do erro: {ex.Message}");
+        }
+
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
         Console.ReadKey();
         Console.Clear();
 
